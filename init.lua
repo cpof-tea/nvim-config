07a94e8e35498983dbb43bebc4dbbfc7ad4b7d0b
Oleg Makarenko <oleg.makarenko@pm.me>
2022-04-09 18:43:29 +0400

    Update colors for base syntax groups

--- init.lua
+++ init.lua
@@ -2,6 +2,16 @@ vim.opt.relativenumber = true
 vim.opt.listchars = { tab = '→ ', space = '•', eol = '↵' }
 vim.opt.signcolumn = 'yes'
 
+vim.cmd([[
+	highlight Comment cterm=none ctermfg=2
+	highlight Statement cterm=bold ctermfg=5
+	highlight Identifier cterm=none ctermfg=none
+	highlight Function cterm=none ctermfg=3
+	highlight Boolean cterm=none ctermfg=4
+	highlight Number cterm=none ctermfg=4
+	highlight String cterm=none ctermfg=1
+]])
+
 require('plugins')
 
 syntaxStack = require('syntaxStack')
