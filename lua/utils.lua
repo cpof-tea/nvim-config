c9e04d5aa5a7bf8e03e0eb6c67a0e393fc5b5a52
Oleg Makarenko <oleg.makarenko@pm.me>
2022-04-16 17:10:51 +0400

    Add `merge` utils

--- lua/utils.lua
+++ lua/utils.lua
@@ -6,4 +6,13 @@ function utils.forEach(table, fn)
 	end
 end
 
+function utils.merge(table1, table2)
+	local newTable = {}
+
+	for key, value in pairs(table1) do newTable[key] = value end
+	for key, value in pairs(table2) do newTable[key] = value end
+
+	return newTable
+end
+
 return utils
